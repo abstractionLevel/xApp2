40aabd4a953409304438ac65220e8edc0cc64f7f
skynet <deluciaugo@gamil.com>
2022-07-18 16:00:30 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from "./src/screens/Home";
+
+
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
+	
+	return (
+		<NavigationContainer>
+     		<Stack.Navigator>
+        		<Stack.Screen
+          			name="Home"
+          			component={Home}
+          			options={{ title: 'Welcome' }}
+        		/>
+      		</Stack.Navigator>
+    	</NavigationContainer>
+	)
+};
+export default App;
---
+++ src/screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NativeRouter, Route, Link } from "react-router-native";
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+  } from 'react-native';
+
+const Home = () => {
+
+    return (
+        <Text>Home</Text>
+    )
+
+};
+
+export default Home;
