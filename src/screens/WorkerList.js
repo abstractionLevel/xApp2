b33cd026ac460478b6097385e54398c5298cc63e
Ugo De Lucia <deluciaugo@gmail.com>
2022-12-01 22:30:43 +0000

    aggiunto FlatList

--- src/screens/WorkerList.js
+++ src/screens/WorkerList.js
@@ -1,20 +1,40 @@
 import React, {useState, useEffect} from 'react'
 import services from '../services'
+import {View, FlatList, Dimensions, AppRegistry,Text} from 'react-native'
 
 const WorkerList = props => {
+    const [workers, setWorkers] = useState(null)
 
     useEffect(() => {
         services
-            .findWorkers({worker: props.route.params.searched, citta: "gorizia"})
-            .then(response => {
-                console.log("resp ", response.data)
+            .findWorkers({
+                worker: props.route.params.searched,
+                citta: 'gorizia',
             })
-            .catch(err=>{
-                console.log("err", err)
+            .then(response => {
+                setWorkers(response.data)
+            })
+            .catch(err => {
+                console.log('err', err)
             })
     }, [props.route.params.searched])
 
-    return <></>
+    const Item = ({item}) => (
+        <View>
+            <Text>{item.type}</Text>
+            <Text>{item.vote}</Text>
+        </View>
+    )
+
+    return (
+        <View>
+            <FlatList 
+                data={workers && workers} 
+                renderItem={({item})=> <Item item={item} />}
+                keyExtractor={item => item.id}    
+                />
+        </View>
+    )
 }
 
 export default WorkerList
