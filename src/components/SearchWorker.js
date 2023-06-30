68647de3918b4437a4d659bdccb7567a9830d26b
ugo <deluciaugo@gmail.com>
2023-06-30 22:09:58 +0200

    spostato il searchWorker nella home

--- src/components/SearchWorker.js
+++ src/components/SearchWorker.js
@@ -42,6 +42,7 @@ const styles = ScaledSheet.create({
         width: '100%',
         alignItems: 'center',
         padding: 10,
+        
 
     },
     inputSearch: {
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Image,
+    TextInput,
+    TouchableOpacity,
+    Text,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView
 } from 'react-native';
 import { ScaledSheet } from 'react-native-size-matters';
 import AddressComponent from '../components/AddressComponent';
@@ -10,30 +16,53 @@ import { Dimensions } from 'react-native';
 
 const Home = () => {
 
-    return (
-        <>
-            <View style={{backgroundColor: 'white'}}>
-                <AddressComponent/>
-                <Image
-                    style={styles.imagePlumbers}
-                    source={require('../../assets/plumbers.jpg')}
-                />
-                <SearchWorker/>
-                <Image
-                    style={styles.imageConnectionWorkers}
-                    source={require('../../assets/connectionWorker2.png')}
-                />
-            </View>
-         
-        
-        </>
-    )
+    const [hideAddress, setHideAddress] = useState(false);
+    const [searched,setSearched] = useState(null)
 
+    return (
+        <View
+            style={styles.container}>
+            {hideAddress === false &&
+                <AddressComponent />
+            }
+            <Image
+                style={styles.imagePlumbers}
+                source={require('../../assets/plumbers.jpg')}
+            />
+            <View style={{ alignItems: 'center', }}>
+                <TextInput
+                    placeholderTextColor="gray"
+                    underlineColorAndroid="white"
+                    style={styles.inputSearch}
+                    placeholder='Cosa Cerchi?'
+                    onFocus={() => setHideAddress(true)}
+                    onBlur={() => setHideAddress(false)}
+
+                onChangeText={(val) => setSearched(val)}
+                value={searched}
+                />
+                <TouchableOpacity
+                    style={styles.buttonSearch}
+                onPress={() => navigation.navigate('WorkerList', { searched: searched })}
+                >
+                    <Text style={styles.buttonText} >Cerca</Text>
+                </TouchableOpacity>
+            </View>
+            <Image
+                style={styles.imageConnectionWorkers}
+                source={require('../../assets/connectionWorker2.png')}
+            />
+        </View>
+    )
 };
 
 var { width, height } = Dimensions.get('window');
 
 const styles = ScaledSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
     imagePlumbers: {
         resizeMode: "contain",
         width: width,
@@ -46,6 +75,36 @@ const styles = ScaledSheet.create({
         marginTop: 20,
         justifyContent: 'center',
         opacity: 0.4,
+    },
+    inputSearch: {
+        width: '90%',
+        height: 60,
+        backgroundColor: '#fff',
+        fontSize: 20,
+        borderTopLeftRadius: 4,
+        borderBottomLeftRadius: 4,
+        borderColor: '#0088ff',
+        borderWidth: 1,
+        paddingLeft: 10,
+
+    },
+    buttonSearch: {
+        marginTop: 4,
+        height: 40,
+        width: '90%',
+        backgroundColor: '#0096FF',
+        borderRadius: 6,
+        justifyContent: 'center',
+    },
+    inputContainer: {
+        marginBottom: '10@s',
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontFamily: 'RobotoBold',
+        fontSize: 18,
+        marginRight: 2,
+        color: 'white',
     },
 
 
