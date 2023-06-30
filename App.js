af90c746615531b6db5800e7c720c22fb852543b
ugo <deluciaugo@gmail.com>
2023-06-30 23:55:42 +0200

    aggiunto l'icona nel head cliccalbile

--- App.js
+++ App.js
@@ -6,12 +6,10 @@
  * @flow strict-local
  */
 
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
-
-// import Home from "./src/screens/Home";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Account from './src/screens/Account'
 import ProfileWorker from './src/screens/ProfileWorker'
 import WorkerList from './src/screens/WorkerList'
@@ -23,72 +21,148 @@ import ChangePassword from './src/screens/ChangePassword'
 import JobProfile from './src/screens/JobProfile'
 import { Provider } from 'react-redux';
 import store from './src/redux/store'
+import {
+    TouchableOpacity,
+    Text
+} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather'
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
 
-    const [auth,setAuth] = useState(null);
+    const [auth, setAuth] = useState(null);
 
     return (
         <Provider store={store}>
-        <AppContext.Provider value={{auth,setAuth}} >
-            <NavigationContainer ref={navigationRef}>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name={'Home'}
-                        component={HomeTabNavigation}
-                        options={{headerShown: false}}
-                    />
-                    <Stack.Screen
-                        name={'Account'}
-                        component={Account}
-                        options={{
-                            headerShown: true,
-                            title: ""
-                        }}
-                    />
-                    <Stack.Screen
-                        name={'SignUp'}
-                        component={SignUp}
-                        options={{
-                            headerShown: false,
-                        }}
-                    />
-					<Stack.Screen
-                        name={'WorkerList'}
-                        component={WorkerList}
-                        options={{
-                            headerShown: true,
-                            title: ""
-                        }}
-                    />
-                    <Stack.Screen
-                        name={'ProfileWorker'}
-                        component={ProfileWorker}
-                        options={{
-                            headerShown: true,
-                            title: ""
-                        }}
-                    />
-                    <Stack.Screen
-                        name={'ChangePassword'}
-                        component={ChangePassword}
-                        options={{
-                            headerShown: true,
-                            title: ""
-                        }}
-                    />
-                    <Stack.Screen
-                        name={'JobProfile'}
-                        component={JobProfile}
-                        options={{
-                            headerShown: true,
-                            title: ""
-                        }}
-                    />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </AppContext.Provider>
+            <AppContext.Provider value={{ auth, setAuth }} >
+                <NavigationContainer ref={navigationRef}>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name={'HomeScreen'}
+                            component={HomeTabNavigation}
+                            options={{ headerShown: false }}
+                        />
+                        <Stack.Screen
+                            name={'Account'}
+                            component={Account}
+                            options={({navigation}) => ({
+                                title: "",
+                                headerRight: () => (
+                                    <TouchableOpacity
+                                        style={{ marginLeft: 16 }}
+                                        onPress={() => navigation.navigate('Home')}
+                                    >
+                                        <Text style={{
+                                            marginRight: 20,
+                                            fontWeight: '900',
+                                            color: 'black',
+                                            fontSize: 20
+
+                                        }}>xApp</Text>
+                                    </TouchableOpacity>
+
+                                ),
+                            })}
+                        />
+                        <Stack.Screen
+                            name={'SignUp'}
+                            component={SignUp}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                        <Stack.Screen
+                            name={'WorkerList'}
+                            component={WorkerList}
+                            options={({navigation}) => ({
+                                title: "",
+                                headerRight: () => (
+                                    <TouchableOpacity
+                                        style={{ marginLeft: 16 }}
+                                        onPress={() => navigation.navigate('Home')}
+                                    >
+                                        <Text style={{
+                                            marginRight: 20,
+                                            fontWeight: '900',
+                                            color: 'black',
+                                            fontSize: 20
+
+                                        }}>xApp</Text>
+                                    </TouchableOpacity>
+
+                                ),
+                            })}
+                        />
+                        <Stack.Screen
+                            name={'ProfileWorker'}
+                            component={ProfileWorker}
+                            options={({navigation}) => ({
+                                title: "",
+                                headerRight: () => (
+                                    <TouchableOpacity
+                                        style={{ marginLeft: 16 }}
+                                        onPress={() => navigation.navigate('Home')}
+                                    >
+                                        <Text style={{
+                                            marginRight: 20,
+                                            fontWeight: '900',
+                                            color: 'black',
+                                            fontSize: 20
+
+                                        }}>xApp</Text>
+                                    </TouchableOpacity>
+
+                                ),
+                            })}
+                        />
+                        <Stack.Screen
+                            name={'ChangePassword'}
+                            component={ChangePassword}
+                            options={({navigation})=>({
+                                title: "",
+                                headerRight: () => (
+                                    <TouchableOpacity
+                                        style={{ marginLeft: 16 }}
+                                        onPress={() => navigation.navigate('Home')}
+                                    >
+                                        <Text style={{
+                                            marginRight: 20,
+                                            fontWeight: '900',
+                                            color: 'black',
+                                            fontSize: 20
+
+                                        }}>xApp</Text>
+                                    </TouchableOpacity>
+
+                                ),
+                            })}
+                        />
+                        <Stack.Screen
+                            name={'JobProfile'}
+                            component={JobProfile}
+                            options={({ navigation }) => ({
+                                title: "",
+                                headerRight: () => (
+                                    <TouchableOpacity
+                                        style={{ marginLeft: 16 }}
+                                        onPress={() => navigation.navigate('Home')}
+                                    >
+                                        <Text style={{
+                                            marginRight: 20,
+                                            fontWeight: '900',
+                                            color: 'black',
+                                            fontSize: 20
+
+                                        }}>xApp</Text>
+                                    </TouchableOpacity>
+
+                                ),
+                            })}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </AppContext.Provider>
         </Provider>
     )
 }
